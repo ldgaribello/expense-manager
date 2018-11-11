f49bf1952237045e80468690b55954ffe48bc0a7
Leon Garibello <ldgaribello@gmail.com>
2018-11-11 12:34:14 -0500

    Add 404 page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -9,15 +9,17 @@ const DashboardPage = () => <div>This is the dashboard page</div>;
 const AddExpensePage = () => <div>This is the add expense page</div>;
 const EditExpensePage = () => <div>This is the edit expense page</div>;
 const HelpPage = () => <div>This is the help page</div>;
+const NotFoundPage = () => <div>404!</div>;
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={DashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 

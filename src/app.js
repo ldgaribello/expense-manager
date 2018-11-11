3b0e78098bd9f0c976eaa455d015927a8d24e3b0
Leon Garibello <ldgaribello@gmail.com>
2018-11-11 12:14:24 -0500

    Add initial  application routing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
+const DashboardPage = () => <div>This is the dashboard page</div>;
+const AddExpensePage = () => <div>This is the add expense page</div>;
+const EditExpensePage = () => <div>This is the edit expense page</div>;
+const HelpPage = () => <div>This is the help page</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={DashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"));

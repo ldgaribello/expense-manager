178110c3fccef7b5629ed5b74586c38bd0c52ac4
Leon Garibello <ldgaribello@gmail.com>
2018-11-12 17:04:43 -0500

    Add basic navigation menu

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
@@ -9,17 +9,43 @@ const DashboardPage = () => <div>This is the dashboard page</div>;
 const AddExpensePage = () => <div>This is the add expense page</div>;
 const EditExpensePage = () => <div>This is the edit expense page</div>;
 const HelpPage = () => <div>This is the help page</div>;
-const NotFoundPage = () => <div>404!</div>;
+
+const NotFoundPage = () => (
+  <div>
+    404! <Link to="/">Go home</Link>
+  </div>
+);
+
+const Header = () => (
+  <header>
+    <h1>Expense Manager</h1>
+    <NavLink activeClassName="is-active" to="/" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink activeClassName="is-active" to="/create">
+      Add expense
+    </NavLink>
+    <NavLink activeClassName="is-active" to="/edit">
+      Edit expense
+    </NavLink>
+    <NavLink activeClassName="is-active" to="/help">
+      Help
+    </NavLink>
+  </header>
+);
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={DashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={DashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 

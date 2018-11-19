f8514e698e765ef73db12c336a8d33651d9fc390
Leon Garibello <ldgaribello@gmail.com>
2018-11-18 23:23:16 -0500

    Add destructuring to redux example

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,21 +2,34 @@ import { createStore } from "redux";
 
 console.log("This is the redux playground...");
 
+const doIncrement = ({ incrementBy = 1 } = {}) => ({
+  type: "INCREMENT",
+  incrementBy
+});
+
+const doDecrement = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+const doSet = ({ count }) => ({
+  type: "SET",
+  count
+});
+
+const doReset = () => ({
+  type: "RESET"
+});
+
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy =
-        typeof action.incrementBy === "number" ? action.incrementBy : 1;
-
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case "DECREMENT":
-      const decrementBy =
-        typeof action.decrementBy === "number" ? action.decrementBy : 1;
-
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case "SET":
       return {
@@ -35,21 +48,9 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5
-});
-store.dispatch({
-  type: "INCREMENT"
-});
-store.dispatch({
-  type: "RESET"
-});
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10
-});
-store.dispatch({
-  type: "SET",
-  count: 200
-});
+store.dispatch(doIncrement({ incrementBy: 5 }));
+store.dispatch(doIncrement());
+store.dispatch(doReset());
+store.dispatch(doDecrement({ decrementBy: 10 }));
+store.dispatch(doDecrement());
+store.dispatch(doSet({ count: 200 }));

8cf62d5af74311c0b969433bffa79330735c7f6b
Leon Garibello <ldgaribello@gmail.com>
2018-12-07 18:23:59 -0500

    Add combine reducer dispatch example

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,11 +1,24 @@
 import { createStore, combineReducers } from "redux";
+import uuid from "uuid";
 
 console.log("This is the expensify reducer...");
+
+const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0
+} = {}) => ({
+  type: "ADD_EXPENSE",
+  expense: { id: uuid(), description, note, amount, createdAt }
+});
 
 // Expenses reducer
 const expensesDefaultState = [];
 const expensesReducer = (state = expensesDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return state.concat(action.expense);
     default:
       return state;
   }
@@ -32,8 +45,11 @@ const store = createStore(
     filters: filtersReducer
   })
 );
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-console.log(store.getState());
+store.dispatch(addExpense({ description: "Rent", amount: 100 }));
 
 const demoState = {
   expenses: [

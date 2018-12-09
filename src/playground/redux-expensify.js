d9e86040a193b755beafe6bae717a21ebc708060
Leon Garibello <ldgaribello@gmail.com>
2018-12-09 14:37:51 -0500

    Add edit expense action

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -15,6 +15,13 @@ const addExpense = ({
   expense: { id: uuid(), description, note, amount, createdAt }
 });
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  id,
+  updates
+});
+
 // REMOVE_EXPENSE
 const removeExpense = ({ id } = {}) => ({
   type: "REMOVE_EXPENSE",
@@ -27,6 +34,17 @@ const expensesReducer = (state = expensesDefaultState, action) => {
   switch (action.type) {
     case "ADD_EXPENSE":
       return [...state, action.expense];
+    case "EDIT_EXPENSE":
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          };
+        } else {
+          return expense;
+        }
+      });
     case "REMOVE_EXPENSE":
       return state.filter(expense => expense.id !== action.id);
     default:
@@ -66,6 +84,7 @@ const expenseTwo = store.dispatch(
   addExpense({ description: "Coffee", amount: 300 })
 );
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 const demoState = {
   expenses: [

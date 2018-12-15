303a4d0418c2fa3f73f67dfd4fca3b3cf7833d68
Leon Garibello <ldgaribello@gmail.com>
2018-12-15 17:50:33 -0500

    Break redux elements into individual files

--- src/app.js
+++ src/app.js
@@ -2,8 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+
+const store = configureStore();
+
+store.dispatch(
+  addExpense({ description: "Rent", amount: 1000, createdAt: -1000 })
+);
+store.dispatch(
+  addExpense({ description: "Coffee", amount: 300, createdAt: 5000 })
+);
+store.dispatch(
+  addExpense({ description: "Diner", amount: 300, createdAt: 3000 })
+);
+store.dispatch(
+  addExpense({ description: "Breakfast", amount: 200, createdAt: 1000 })
+);
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById("app"));

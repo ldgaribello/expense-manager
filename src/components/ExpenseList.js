f06ac7475010f66af1c0a1d5366ab0e5cdb9d42b
Leon Garibello <ldgaribello@gmail.com>
2019-02-22 23:56:30 -0500

    Render individual expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import ExpenseListItem from "./ExpenseListItem";
+
 const ExpenseList = props => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.length}
+    {props.expenses.map(expense => (
+      <ExpenseListItem key={expense.id} {...expense} />
+    ))}
   </div>
 );
 

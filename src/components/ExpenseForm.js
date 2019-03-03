708b035c64feb58c9e2b3cf376b9a4570da0b53e
Leon Garibello <ldgaribello@gmail.com>
2019-03-03 12:46:11 -0500

    Add date picker to expense form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,16 +1,24 @@
 import React from "react";
 
+import moment from "moment";
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+
 class ExpenseForm extends React.Component {
   state = {
     description: "",
     amount: "",
-    note: ""
+    note: "",
+    createdAt: moment(),
+
+    calendarFocused: false
   };
 
   onDescriptionChange = e => {
     const description = e.target.value;
     this.setState(() => ({ description: description }));
   };
+
   onAmountChange = e => {
     const amount = e.target.value;
 
@@ -18,9 +26,18 @@ class ExpenseForm extends React.Component {
       this.setState(() => ({ amount }));
     }
   };
+
+  onDateChange = createdAt => {
+    this.setState(() => ({ createdAt }));
+  };
+
   onNoteChange = e => {
     const note = e.target.value;
     this.setState(() => ({ note }));
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
   };
 
   render() {
@@ -44,6 +61,15 @@ class ExpenseForm extends React.Component {
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt} // momentPropTypes.momentObj or null
+            onDateChange={this.onDateChange} // PropTypes.func.isRequired
+            focused={this.state.calendarFocused} // PropTypes.bool
+            onFocusChange={this.onFocusChange} // PropTypes.func.isRequired
+            id="your_unique_id" // PropTypes.string.isRequired,
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <button>Save</button>
         </form>
